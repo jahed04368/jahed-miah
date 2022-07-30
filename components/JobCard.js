f4b9a99f97cf35e6a7a53a1a38c5859d11dcd47e
Jahed Miah <jahed04368@gmail.com>
2022-07-30 10:58:06 +0100

    less padding on jobcard component

--- components/JobCard.js
+++ components/JobCard.js
@@ -69,8 +69,8 @@ const JobCard = ({
           <div className="grid grid-cols-3 col-span-3 sm:grid-cols-4 sm:col-span-4 md:grid-cols-5 md:col-span-5 lg:grid-cols-7 lg:col-span-7 py-2">
             {techUsed.map((tech) => {
               return (
-                <div className="py-2" key={id}>
-                  <span className="bg-blue-100 text-blue-800 text-base font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
+                <div className="py-1" key={id}>
+                  <span className="bg-blue-100 text-blue-800 text-sm md:text-base font-semibold mr-0 px-1 md:px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
                     {tech}
                   </span>
                 </div>
