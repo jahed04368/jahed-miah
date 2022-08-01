f5748a59cc4a87ca4510d0f700667ef32e634cd8
Jahed Miah <jahed04368@gmail.com>
2022-08-01 11:57:41 +0100

    added  next image

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import JobCard from '../components/JobCard';
 import React, { useState } from 'react';
+import NextImage from 'next/image';
+import JahedsPic from '../public/images/jahed-pic.png';
 
 export default function Home({ data }) {
   const [isExpanded, setExpand] = useState(false);
@@ -20,38 +22,51 @@ export default function Home({ data }) {
     <div className="bg-slate-700">
       <Navbar />
       <div className="text-white container flex flex-wrap justify-between items-center mx-auto px-2 py-10 pt-28">
-        <section className="">
-          <div className="flex">
-            <h2 className="text-3xl mt-2 text-yellow-400">
-              {"Hello, I'm Jahed."}
-            </h2>
-            <a className="text-5xl pb-5 pl-2 text-white">{'😁👋🏼'}</a>
+        <div className="flex w-full md:w-11/12">
+          <section className="w-full">
+            <div className="flex">
+              <h2 className="text-3xl mt-2 text-yellow-400">
+                {"Hello, I'm Jahed."}
+              </h2>
+              <a className="text-5xl pb-5 pl-2 text-white">{'😁👋🏼'}</a>
+            </div>
+            <div className="flex text-xl inline	w-10/12 md-w-full">
+              <span>
+                {`Im a software `}
+                <span className="text-green-300 text-2xl">T</span>
+                <span>
+                  {` Shaped developer at Ostmodern, and living in London, UK.`}
+                </span>
+              </span>
+            </div>
+          </section>
+          <div className="pt-3">
+            <NextImage
+              src={JahedsPic}
+              className="rounded-full"
+              width={120}
+              height={150}
+            />
+            <div className="flex justify-center">
+              <a
+                href="https://github.com/jahed04368"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <AiOutlineGithub size={30} className="hover:text-yellow-400" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/jahed-miah04368/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <AiFillLinkedin size={30} className="hover:text-yellow-400" />
+              </a>
+            </div>
           </div>
-          <div className="flex flex-wrap text-xl ">
-            <h4>{`I'm a software`}</h4>
-            <TbLetterT size={25} className="text-green-400" />
-            <h4>{'Shaped developer at Ostmodern, and'}</h4>
-            <h4>{'living in London, UK.'}</h4>
-          </div>
-        </section>
-        <div className="sm:flex-wrap md:flex">
-          <a
-            href="https://github.com/jahed04368"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <AiOutlineGithub size={30} className="hover:text-yellow-400" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/jahed-miah04368/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <AiFillLinkedin size={30} className="hover:text-yellow-400" />
-          </a>
         </div>
       </div>
-      <div className="container flex flex-wrap justify-between items-center mx-auto  px-2 py-10">
+      <div className="container flex flex-wrap justify-between items-center mx-auto px-2 py-10">
         <div className="flex">
           <h1 className="text-yellow-400 text-2xl">{'About me'}</h1>
           <div className="pl-1">
@@ -62,8 +77,8 @@ export default function Home({ data }) {
           <p
             className={`${
               isExpanded
-                ? `text-white text-lg m-2  md:line-clamp-none line-clamp-none`
-                : `text-white text-lg m-2  md:line-clamp-none line-clamp-5`
+                ? `text-white text-lg m-2 md:line-clamp-none line-clamp-none`
+                : `text-white text-lg m-2 md:line-clamp-none line-clamp-5`
             }`}
             ref={setTrunicatedWrapper}
           >
@@ -79,7 +94,7 @@ export default function Home({ data }) {
           )}
         </div>
         <div className="py-2 flex">
-          <h1 className="text-yellow-400 text-2xl ">{'Experience'}</h1>
+          <h1 className="text-yellow-400 text-2xl">{'Experience'}</h1>
           <div className="pl-1 pr-1 text-green-400 mt-0.5">
             <MdOutlineHistoryEdu size={30} />
           </div>
