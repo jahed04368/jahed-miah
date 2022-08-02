0e5546d52b6d9fcbcdcedaa7ee3e43ebf5cc2ec6
Jahed Miah <jahed04368@gmail.com>
2022-08-02 15:51:55 +0100

    projectCard with test data

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -3,6 +3,21 @@ import Footer from '../../components/Footer';
 import Navbar from '../../components/Navbar';
 import ProjectCard from '../../components/ProjectCard';
 
+const projects = [
+  {
+    id: '1',
+    desc: `This is a React.js app which records all the To Do's you have on your localStorage. You can also remove, search and hide all the To Do's you have.`,
+    projectName: 'To-Do App',
+    urllink: 'https://todo-jahed04368.netlify.app/',
+  },
+  {
+    id: '2',
+    desc: `This is a React.js app which records all the To Do's you have on your localStorage. You can also remove, search and hide all the To Do's you have.`,
+    projectName: 'To-Do App',
+    urllink: 'https://todo-jahed04368.netlify.app/',
+  },
+];
+
 const Projects = () => {
   return (
     <div className="bg-slate-700">
@@ -12,7 +27,19 @@ const Projects = () => {
           {"Here are few projects I've worked on."}
         </h1>
 
-        <ProjectCard />
+        {projects.map((project) => {
+          console.log(project);
+          {
+            return (
+              <ProjectCard
+                key={project.id}
+                desc={project.desc}
+                projectName={project.projectName}
+                urllink={project.urllink}
+              />
+            );
+          }
+        })}
       </div>
       <Footer />
     </div>
