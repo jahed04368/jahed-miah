8ef487e89a6df6b7f0a9738645abdb88fc178916
Jahed Miah <jahed04368@gmail.com>
2022-08-03 11:14:29 +0100

    added new and old projects on projects page and dynamic image url

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import DimensionToggle from './Toggle';
 import TodoAppImage from '../public/images/to-do-app/todo-app.jpeg';
+import HangManGame from '../public/images/hangman-game/hangman-game.jpeg';
+
 import NextImage from 'next/image';
 import Link from 'next/link';
 
-const ProjectCard = ({ desc, projectName, urllink }) => {
+const ProjectCard = ({ img, desc, projectName, urllink }) => {
   return (
     <div className="w-full p-2 ">
       <div className="transform hover:scale-[1.01] transition-all rounded-xl w-full md:w-full bg-gradient-to-r p-1 from-[#D8B4FE] to-[#818CF8]">
@@ -27,7 +28,7 @@ const ProjectCard = ({ desc, projectName, urllink }) => {
               <p className="text-lg text-white pt-2 pr-2.5">{desc}</p>
             </div>
             <div className="row-span-1 md:col-span-1 py-3 ">
-              <NextImage src={TodoAppImage} width={600} height={350} />
+              <NextImage src={`/${img}`} width={600} height={350} />
             </div>
           </div>
         </div>
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -3,41 +3,105 @@ import Footer from '../../components/Footer';
 import Navbar from '../../components/Navbar';
 import ProjectCard from '../../components/ProjectCard';
 
-const projects = [
-  {
-    id: '1',
-    desc: `This is a React.js app which records all the To Do's you have on your localStorage. You can also remove, search and hide all the To Do's you have.`,
-    projectName: 'To-Do App',
-    urllink: 'https://todo-jahed04368.netlify.app/',
-  },
-  {
-    id: '2',
-    desc: `This is a React.js app which records all the To Do's you have on your localStorage. You can also remove, search and hide all the To Do's you have.`,
-    projectName: 'To-Do App',
-    urllink: 'https://todo-jahed04368.netlify.app/',
-  },
-];
+const projects = {
+  newProjects: [
+    {
+      id: '1',
+      desc: `This is a React.js app which records all the To Do's you have on your localStorage. You can also remove, search and hide all the To Do's you have.`,
+      projectName: 'To-Do App',
+      urllink: 'https://todo-jahed04368.netlify.app/',
+      img: '../public/images/to-do-app/todo-app.jpeg',
+    },
+    {
+      id: '2',
+      desc: `This is a React.js app which records all the To Do's you have on your localStorage. You can also remove, search and hide all the To Do's you have.`,
+      projectName: 'Hang Man Game',
+      urllink: 'https://hangman-jahed04368.netlify.app/',
+      img: '../public/images/hangman-game/hangman-game.jpeg',
+    },
+    {
+      id: '3',
+      desc: `This is a React.js app which records all the To Do's you have on your localStorage. You can also remove, search and hide all the To Do's you have.`,
+      projectName: 'Notes App',
+      urllink: 'https://notes-app-jahed04368.netlify.app/',
+      img: '../public/images/notes-app/notes-app.jpeg',
+    },
+  ],
+  oldProjects: [
+    {
+      id: '1',
+      desc: `This is a React.js app which records all the To Do's you have on your localStorage. You can also remove, search and hide all the To Do's you have.`,
+      projectName: 'Dice Game App',
+      urllink: 'https://jahed04368.github.io/dicee-game/',
+      img: '../public/images/dice-game/dice-game.jpeg',
+    },
+    {
+      id: '1',
+      desc: `This is a React.js app which records all the To Do's you have on your localStorage. You can also remove, search and hide all the To Do's you have.`,
+      projectName: 'Drum Kit App',
+      urllink: 'https://jahed04368.github.io/drum-kit/',
+      img: '../public/images/drum-kit/drum-kit.jpeg',
+    },
+    {
+      id: '1',
+      desc: `This is a React.js app which records all the To Do's you have on your localStorage. You can also remove, search and hide all the To Do's you have.`,
+      projectName: 'Simon Game',
+      urllink: 'https://jahed04368.github.io/simon-game/',
+      img: '../public/images/simon-game/simon-game.jpeg',
+    },
+  ],
+};
 
 const Projects = () => {
+  const { newProjects } = projects;
+  const { oldProjects } = projects;
+  console.log(newProjects);
   return (
     <div className="bg-slate-700">
       <Navbar />
-      <div className=" container flex flex-wrap justify-center items-center mx-auto px-2 py-10 lg:pt-20">
+      <div className="container flex flex-wrap justify-center items-center mx-auto px-2 py-10 lg:pt-20">
         <h1 className="text-3xl text-white pb-2">
-          {"Here are few projects I've worked on."}
+          {
+            "Here are few projects I've worked on using NODE.JS, EXPRESS.JS. NEXT.js and REACT"
+          }
         </h1>
 
-        {projects.map((project) => {
+        {newProjects.map((project) => {
           console.log(project);
           {
-            return (
-              <ProjectCard
-                key={project.id}
-                desc={project.desc}
-                projectName={project.projectName}
-                urllink={project.urllink}
-              />
-            );
+            {
+              return (
+                <ProjectCard
+                  key={project.id}
+                  desc={project.desc}
+                  projectName={project.projectName}
+                  urllink={project.urllink}
+                  img={project.img}
+                />
+              );
+            }
+          }
+        })}
+      </div>
+      <div className="container flex flex-wrap justify-center items-center mx-auto px-2 py-10 lg:pt-20">
+        <h1 className="text-3xl text-white pb-2">
+          {"Here are few projects I've worked on using JS, CSS and HTML"}
+        </h1>
+
+        {oldProjects.map((project) => {
+          console.log(project);
+          {
+            {
+              return (
+                <ProjectCard
+                  key={project.id}
+                  desc={project.desc}
+                  projectName={project.projectName}
+                  urllink={project.urllink}
+                  img={project.img}
+                />
+              );
+            }
           }
         })}
       </div>
